0be2967e1c2123e29e9d094eb8365ea7e9cece9a
AliuEEE <aliuom1996@gmail.com>
2021-01-23 15:06:11 +0100

    completed code

--- magic_ball/lib/main.dart
+++ magic_ball/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,12 +22,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int buttonNumber = 1;
   @override
   Widget build(BuildContext context) {
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget> [
-        Image.asset('images/ball1.png'),
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: (){
+              setState(() {
+                buttonNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$buttonNumber.png'),
+          ),
+        ),
       ],
     );
   }
